29d9d6c7ff7a06ea5573eb76b3d5555f749dddbd
Illia Datsenko <datsenko.i@gmail.com>
2023-04-13 22:12:05 +0300

    убрал лишние комментарии

--- src/js/render-trending.js
+++ src/js/render-trending.js
@@ -1,10 +1,7 @@
 import { ThemoviedbAPI } from './themoviedb-api';
 import { changeGenresIdToName } from './change-genres-id-to-name';
 
-const searchFormEl = document.querySelector('.js-search-form');
-
 export async function renderTrending() {
-  // event.preventDefault();
   const themoviedbAPI = new ThemoviedbAPI();
   try {
     const { data } = await themoviedbAPI.getTrending();
