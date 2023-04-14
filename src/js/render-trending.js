c73c145ebbb6ba0568f67a92aa622e4675e5c9c2
Illia Datsenko <datsenko.i@gmail.com>
2023-04-14 13:35:59 +0300

    рендер карток в оаремий файл

--- src/js/render-trending.js
+++ src/js/render-trending.js
@@ -1,12 +1,16 @@
 import { ThemoviedbAPI } from './themoviedb-api';
 import { changeGenresIdToName } from './change-genres-id-to-name';
+import createFilmsCard from '../templates/gallery-card.hbs';
+
+const galleryListEl = document.querySelector('.film__gallery');
 
 export async function renderTrending() {
   const themoviedbAPI = new ThemoviedbAPI();
+  themoviedbAPI.page = 1; // змінювати пагінацією
   try {
     const { data } = await themoviedbAPI.getTrending();
-    changeGenresIdToName(data);
-    console.log(data);
+    await changeGenresIdToName(data);
+    galleryListEl.innerHTML = createFilmsCard(data.results);
   } catch (err) {
     console.log(err);
   }
