7bc85edae8cdadde9f30789fb194291b0ec0501f
Zerossik <zeross19940303@gmail.com>
2023-04-14 19:20:20 +0300

    modal

--- src/js/modal-movie.js
+++ src/js/modal-movie.js
@@ -2,6 +2,7 @@ import * as BasicLightBox from 'basiclightbox';
 import { ThemoviedbAPI } from './themoviedb-api';
 
 const liEl = document.querySelector('.card-list__item');
+const modalEl = document.querySelector('.modal-movie');
 const api = new ThemoviedbAPI();
 
 export const hendlerClickCard = evt => {
@@ -17,14 +18,14 @@ export const hendlerClickCard = evt => {
 function createModalWindow(data) {
   return `
   
-  <img src="https://image.tmdb.org/t/p/w500${data.poster_path}" class=""modal-movie__img />
-  <h2>I need header</h2>
-  <p><span></span></p>
-  <p><span></span></p>
-  <p><span></span></p>
-  <p><span></span></p>
+  <img src="https://image.tmdb.org/t/p/w400${data.poster_path}" class=""modal-movie__img />
+  <h2>${data.original_title}</h2>
+  <p>Vote/Votes<b>${data.vote_average} / ${data.vote_count}</b></p>
+  <p>popularity <b>${data.popularity}</b></p>
+  <p>original_title<b>${data.original_title}</b></p>
+  <p>genres<b></b></p>
   <h3>About</h3>
-  <p>discr</p>
+  <p>${data.overview}</p>
   <div>
   <button type="button">add to Watched</button>
   <button type="button">add to queue</button>
@@ -40,7 +41,6 @@ function getMovieId(id) {
   api
     .getMovieDetails()
     .then(({ data }) => {
-      console.log(data);
       console.log(createModalWindow(data));
     })
     .catch(err => console.warn(err));
