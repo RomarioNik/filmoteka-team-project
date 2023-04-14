f8c2f14969e98ffcfb2b7c75b0e1119a4820109e
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-04-15 02:12:34 +0300

    genres length

---
+++ src/js/change-genres-id-to-name.js
@@ -0,0 +1,25 @@
+import { ThemoviedbAPI } from './themoviedb-api';
+
+export async function changeGenresIdToName(movies) {
+  const themoviedbAPI = new ThemoviedbAPI();
+  try {
+    const genresListResponce = await themoviedbAPI.getGenres();
+    const genresIdList = genresListResponce.data.genres;
+    movies.results.forEach(element => {
+      const array = element.genre_ids;
+      //console.log(genresIdList);
+      if (!array) return 'Other';
+      const genresNames = array.map(elem => {
+        if (genresIdList.find(x => x.id === elem)) {
+          return genresIdList.find(x => x.id === elem).name;
+        } else {
+          return 'Other';
+        }
+      });
+
+      element.genre_ids = genresNames;
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
--- src/js/change-genres-length.js
+++ src/js/change-genres-length.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { ThemoviedbAPI } from './themoviedb-api';
 
 export async function changeGenresLength(movies) {
@@ -28,3 +29,38 @@ return 'Other';
       console.log(err);
     }
   }
+=======
+// import { ThemoviedbAPI } from './themoviedb-api';
+// import { changeGenresIdToName } from './change-genres-id-to-name';
+
+export async function changeGenresLength(movies) {
+  // const themoviedbAPI = new ThemoviedbAPI();
+  
+  
+  try {
+
+  // const genresListResponce = await themoviedbAPI.getGenres();
+  // //const genresIdList = genresListResponce.data.genres;
+  //await changeGenresIdToName(data)
+  movies.results.forEach(element => {
+    const maxLength = 2;
+    const array = element.genre_ids;
+    
+    //console.log(array);
+    if (array.length > maxLength) {
+      let newArray = array.slice(0, 2);
+      
+      console.log(newArray);
+      return newArray.push('Other')
+    } else {
+      console.log(array);
+      return array
+    }
+  })    
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
+>>>>>>> Stashed changes
