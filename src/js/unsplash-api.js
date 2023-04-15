11f43cbf9000196a39c7663443b9c5b9ab17e57f
YevhenBez <byxgalter@bigmir.net>
2023-04-15 13:37:19 +0300

    В импорте добавил расширение js    , убрал const    у значений в Классе    class UnsplashAPI    и добавил     this     для    options      в     pagination      Проект по JS
    В импорте  import Pagination from 'tui-pagination';   добавил расширение js   потому что VScode   не мог найти данный файл. Убрал const    у значений в Классе    class UnsplashAPI      (потому как VScode  писал, что   const   неприемлемо в   class UnsplashAPI   ) и добавил     this     для    options      в     pagination   (это уже отсебятина)     Проект по JS

--- src/js/unsplash-api.js
+++ src/js/unsplash-api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-import Pagination from 'tui-pagination';
+import Pagination from 'tui-pagination.js';
 import 'tui-pagination/dist/tui-pagination.css';
 
 import { ThemoviedbAPI } from './themoviedb-api';
 
 export class UnsplashAPI {
-    const options = {
+    options = {
         totalItems: 1000,    
         itemsPerPage: 20,
         visiblePages: 20,
@@ -17,5 +17,5 @@ export class UnsplashAPI {
 
     page = this.options.page;
 
-    const pagination = new Pagination('pagination', options);
+    pagination = new Pagination('pagination', this.options);
 }
