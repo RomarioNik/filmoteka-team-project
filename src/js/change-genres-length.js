9f6bdccacaf8ec8075aaf48b335a08543c71037b
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-04-15 13:49:33 +0300

    genres_name_length

--- src/js/change-genres-length.js
+++ src/js/change-genres-length.js
@@ -1,16 +1,24 @@
 
-// import { ThemoviedbAPI } from './themoviedb-api';
-// import { changeGenresIdToName } from './change-genres-id-to-name';
+import { ThemoviedbAPI } from './themoviedb-api';
+import { changeGenresIdToName } from './change-genres-id-to-name';
+
+
+
+
+
+
+
 
 export async function changeGenresLength(movies) {
-  // const themoviedbAPI = new ThemoviedbAPI();
-  
-  
+const themoviedbAPI = new ThemoviedbAPI();
+   
   try {
-
-  // const genresListResponce = await themoviedbAPI.getGenres();
-  // //const genresIdList = genresListResponce.data.genres;
-  //await changeGenresIdToName(data)
+    await changeGenresIdToName(movies);
+  const genresResponce = await themoviedbAPI.getGenres();
+  //console.log(genresResponce);
+  const genresIdList = genresResponce.data.genres;
+  //console.log(genresIdList);
+  //const genresName = 
   movies.results.forEach(element => {
     const maxLength = 2;
     const array = element.genre_ids;
@@ -18,13 +26,21 @@ export async function changeGenresLength(movies) {
     //console.log(array);
     if (array.length > maxLength) {
       let newArray = array.slice(0, 2);      
+     
+      newArray.push('Other')
       console.log(newArray);
-      return newArray.push('Other')
+      const genresLongNames = newArray.join(', ');
+      console.log(genresLongNames);
+      return genresLongNames
+      
     } else {
       console.log(array);
-      return array
+      const genresShotNanes= array.join(', ')
+      console.log(genresShotNanes)
+      return genresShotNanes;
     }
-  })    
+  })
+     
   } catch (err) {
     console.log(err);
   }
