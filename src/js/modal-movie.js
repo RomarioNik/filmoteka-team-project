433595272814f8ce9936febd8bf87aee406da38e
Zerossik <zeross19940303@gmail.com>
2023-04-16 01:40:16 +0300

    modal-movie beta

--- src/js/modal-movie.js
+++ src/js/modal-movie.js
@@ -1,43 +1,54 @@
 import * as BasicLightBox from 'basiclightbox';
 import { ThemoviedbAPI } from './themoviedb-api';
-
-const instance = BasicLightBox.create(`<div class="modal-movie"></div>`);
 const liEl = document.querySelector('.card-list__item');
-const modalEl = document.querySelector('.modal-movie');
-const api = new ThemoviedbAPI();
 
 export const hendlerClickCard = evt => {
   // проверяю, клик по карточке или нет.
   if (evt.currentTarget.nodeName != 'LI') {
     return;
   }
+  modalIsOpen();
   // открывает модальное окно!
-  const instance = BasicLightBox.create('<div class="modal-movie"></div>');
-  instance.show();
-  api.movie_id = 920;
-  api
-    .getMovieDetails()
-    .then(({ data }) => {})
-    .catch(err => console.warn(err));
 };
 // создает разметку модалки и вызывается в экземпляре BasicLightBox
 function createModalWindow(data) {
   return `
   
-  <img src="https://image.tmdb.org/t/p/w400${data.poster_path}" class=""modal-movie__img />
-  <h2>${data.original_title}</h2>
+  <img src="https://image.tmdb.org/t/p/w400${data.poster_path}" class="modal-movie__img" alt="${data.original_title}" />
+  <h2 class="modal-movie__title">${data.original_title}</h2>
+  <div class="modal-movie__wrap-text">
   <p>Vote/Votes<b>${data.vote_average} / ${data.vote_count}</b></p>
-  <p>popularity <b>${data.popularity}</b></p>
+  <p>popularity<b>${data.popularity}</b></p>
   <p>original_title<b>${data.original_title}</b></p>
   <p>genres<b></b></p>
-  <h3>About</h3>
-  <p>${data.overview}</p>
-  <div>
-  <button type="button">add to Watched</button>
-  <button type="button">add to queue</button>
   </div>
+  <h3 class="modal-movie__about">About</h3>
+  <p>${data.overview}</p>
+ 
+  <button type="button" class="modal-movie__Watch">add to Watched</button>
+  <button type="button" class="modal-movie__queue">add to queue</button>
+  
   
   `;
 }
 
+function getDateFromId(id) {
+  const api = new ThemoviedbAPI();
+  const modalEl = document.querySelector('.modal-movie');
+  api.movie_id = id;
+  api
+    .getMovieDetails()
+    .then(({ data }) => {
+      console.log(modalEl);
+      modalEl.insertAdjacentHTML('afterbegin', createModalWindow(data));
+    })
+    .catch(err => console.warn(err));
+}
+
+function modalIsOpen() {
+  const instance = BasicLightBox.create('<div class="modal-movie"></div>');
+  instance.show();
+
+  getDateFromId(920);
+}
 liEl.addEventListener('click', hendlerClickCard);
