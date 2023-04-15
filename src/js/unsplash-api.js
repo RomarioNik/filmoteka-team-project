942a811bca79e3205b9ab896746eacae7c543181
YevhenBez <byxgalter@bigmir.net>
2023-04-15 11:09:33 +0300

    Сделал опшины для библиотеки   tui.pagination     Проект по JS
    Сделал опшины для библиотеки   tui.pagination     Проект по JS

--- src/js/unsplash-api.js
+++ src/js/unsplash-api.js
@@ -4,5 +4,10 @@ import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 
 export class UnsplashAPI {
-    
+    const options = {
+        totalItems: 1000,    
+        itemsPerPage: 20,
+        visiblePages: 20,
+        page: 1
+    }
 }
