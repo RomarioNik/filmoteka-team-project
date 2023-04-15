4d6dc11e2dc2521d758d8bb986ce1f486a65b836
Roman <rprikhoda@ukr.net>
2023-04-15 20:30:53 +0300

    update

--- src/js/firebase/firebase.js
+++ src/js/firebase/firebase.js
@@ -1,7 +1,7 @@
 import { handleClickTestBtn } from './handle/handleClickTestBtn.js';
 import refsEl from '../refs.js';
 
-const refs = refsEl();
+// const refs = refsEl();
 
 // вешаем слушателя на кнопку открывающую модальное окно с формой.
 export const firebase = () => {};
