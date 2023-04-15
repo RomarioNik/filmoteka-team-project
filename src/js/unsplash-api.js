cce1de97729472aaafbe6f358d3da87eb119341e
YevhenBez <byxgalter@bigmir.net>
2023-04-15 23:24:06 +0300

    Убрал   class UnsplashAPI {} не нужные параметры и прописал    const   у    options   и    pagination     Проект по JS
    Убрал   class UnsplashAPI {} не нужные параметры и прописал    const   у    options   и    pagination     (это параметры библиотеки    tui.pagination    ) по причине изменения реализации логики листалки (пагинации)     Проект по JS

--- src/js/unsplash-api.js
+++ src/js/unsplash-api.js
@@ -5,17 +5,11 @@ import 'tui-pagination/dist/tui-pagination.css';
 
 import { ThemoviedbAPI } from './themoviedb-api';
 
-export class UnsplashAPI {
-    options = {
+const options = {
         totalItems: 1000,    
         itemsPerPage: 20,
         visiblePages: 20,
         page: 1
     }
 
-    query = null;
-
-    page = this.options.page;
-
-    pagination = new Pagination('pagination', this.options);
-}
+const pagination = new Pagination('pagination', this.options);
