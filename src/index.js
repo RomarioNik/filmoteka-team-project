cffddc1b55e30aa920dcb2be44285754489a771f
YevhenBez <byxgalter@bigmir.net>
2023-04-16 01:44:05 +0300

    В файле   index.js    сделал импорт колбека для Пагинации, нашёл Дивчик с кнопками для Пагинации и повесил слушателя при клике на Дивчик. В файле    unsplash-api.js      сделал все импорты для функции Рендера, прописал функцию Рендера, прописал колбек для клика по Дивчику Пагинации и сразу его экспортировал. Проект по JS
    В файле   index.js    сделал импорт колбека для Пагинации, нашёл Дивчик с кнопками для Пагинации и повесил слушателя при клике на Дивчик Пагинации. В файле    unsplash-api.js      сделал все импорты для функции Рендера    (      renderTrendingPagination()        )  , прописал функцию Рендера, прописал колбек для клика по Дивчику Пагинации (     handleLoadNextPaginationPage (event)      ) и сразу его экспортировал. Проект по JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import { renderTrending } from './js/render-trending';
 
 renderTrending();
+
+
+
+import { handleLoadNextPaginationPage } from './js/unsplash-api';
+
+const btnPagination = document.querySelector('.tui-pagination');
+
+btnPagination.addEventListener('click', handleLoadNextPaginationPage);
--- src/js/unsplash-api.js
+++ src/js/unsplash-api.js
@@ -5,7 +5,10 @@ import 'tui-pagination/dist/tui-pagination.css';
 
 import { ThemoviedbAPI } from './themoviedb-api';
 
-const btnPagination = document.querySelector('.tui-pagination');
+import { changeGenresIdToName } from './change-genres-id-to-name';
+import createFilmsCard from '../templates/gallery-card.hbs';
+
+const galleryListEl = document.querySelector('.film__gallery');
 
 const options = {
         totalItems: 1000,    
@@ -15,3 +18,24 @@ const options = {
     }
 
 const pagination = new Pagination('pagination', options);
+
+async function renderTrendingPagination() {
+        const themoviedbAPI = new ThemoviedbAPI();
+        themoviedbAPI.page = pagination._currentPage; // змінювати пагінацією
+
+    console.log(themoviedbAPI.page);
+    
+    try {
+    const { data } = await themoviedbAPI.getTrending();
+    await changeGenresIdToName(data);
+    galleryListEl.innerHTML = createFilmsCard(data.results);
+    } catch (err) {
+    console.log(err);
+    }
+}
+
+export function handleLoadNextPaginationPage (event) {
+    console.log(pagination._currentPage);
+    
+    renderTrendingPagination();
+};
