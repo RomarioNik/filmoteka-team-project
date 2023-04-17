d17b5a7c5ee9449adc5ce8216a323a40d7b9ac0b
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2023-04-18 02:19:30 +0300

    Update proverkaSettingsQueue.js

--- src/js/proverkaSettingsQueue.js
+++ src/js/proverkaSettingsQueue.js
@@ -1,8 +1,13 @@
-export function searchingInfoButtonQueue (btn) {
-    const buttonProverka = btn.dataset.id
-    const localeArrayProverka = localStorage.getItem('historyQueue')
-    if (localeArrayProverka.includes(buttonProverka)){
-    return true   
-    }
-    return false
-    }
+export function searchingInfoButtonQueue(btn) {
+  const buttonProverka = btn.dataset.id;
+  const localeArrayProverka = JSON.parse(localStorage.getItem('historyQueue'));
+  if (localeArrayProverka === null) {
+    localStorage.setItem('historyQueue', JSON.stringify([]));
+    return false;
+  }
+
+  if (localeArrayProverka.includes(buttonProverka)) {
+    return true;
+  }
+  return false;
+}
