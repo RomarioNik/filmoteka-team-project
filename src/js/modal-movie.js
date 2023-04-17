89baa7ea709f904b6e083eebe2e56474b74e24de
Zerossik <zeross19940303@gmail.com>
2023-04-17 15:26:17 +0300

    modal-movie mobile beta

--- src/js/modal-movie.js
+++ src/js/modal-movie.js
@@ -2,6 +2,7 @@ import * as BasicLightBox from 'basiclightbox';
 import { ThemoviedbAPI } from './themoviedb-api';
 import { ulAdd } from './modal-dorabotka';
 const ulEll = document.querySelector('.film__gallery');
+
 export const hendlerClickCard = event => {
   // проверяю, клик по карточке или нет.
   if (event.target.nodeName !== 'UL') {
@@ -12,38 +13,37 @@ export const hendlerClickCard = event => {
 };
 // создает разметку модалки и вызывается в экземпляре BasicLightBox
 function createModalWindow(data) {
-  return ` 
-  <button class="close-button-modal" type='button' width="20" height="20 font-size="20"> 
-X 
+  return `
+  <button class="modal-movie__btn-close" data-close type='button' > 
+X
 </button> 
  
   <img src="https://image.tmdb.org/t/p/w400${
     data.poster_path
   }" class="modal-movie__img" alt="${data.original_title}" /> 
   <h2 class="modal-movie__title">${data.original_title}</h2> 
-  <div class="modal-movie__wrap"> 
-    <div class="modal-movie__wrap-key"> 
-    <p>Vote/Votes</p> 
-    <p>popularity</p> 
-    <p>original_title</p> 
-    <p>genres</p> 
-     </div> 
-     <div class="modal-movie__wrap-value"> 
-    <p><span>${data.vote_average.toFixed(1)}</span> / ${data.vote_count}</p> 
-    <p>${data.popularity.toFixed(1)}</p> 
-    <p>${data.original_title}</p> 
-    <p></p> 
-   </div> 
-  </div> 
+<ul class=modal-movie__list>
+<li class="movie-modal__list-item"><p>Vote/Votes</p><span class="active">${data.vote_average.toFixed(
+    1
+  )}</span> / <span>${data.vote_count}</span></li>
+<li class="movie-modal__list-item"><p>popularity </p><span>${data.popularity.toFixed(
+    1
+  )}</span> </li>
+<li class="movie-modal__list-item"><p>Title</p> <span>${
+    data.original_title
+  }</span></li>
+<li class="movie-modal__list-item"><p>genres</p> </li>
+</ul>
+
   <h3 class="modal-movie__about">About</h3> 
   <p class="modal-movie__desc">${data.overview}</p> 
  <div class="modal-movie__btn-wrap"> 
   <button type="button" class="modal-movie__Watch" data-id=${
     data.id
-  }>add to Watched</button> 
+  } data-btnname="watched">add to Watched</button> 
   <button type="button" class="modal-movie__queue" data-id=${
     data.id
-  }>add to queue</button> 
+  } data-btnname="queue">add to queue</button> 
   </div> 
    
   `;
@@ -63,9 +63,28 @@ async function getDateFromId(id) {
   }
 }
 
-function modalIsOpen(ids) {
+async function modalIsOpen(ids) {
   const instance = BasicLightBox.create('<div class="modal-movie"></div>');
   instance.show();
-  getDateFromId(ids);
+  await getDateFromId(ids);
+  if (instance.visible()) {
+    const closeBtn = document.querySelector('[data-close]');
+    closeBtn.addEventListener('click', () => {
+      instance.close();
+    });
+    // window.addEventListener('keydown', evt => {
+    //   console.log('click');
+    //   if (evt.code === 'Escape') {
+    //     instance.close();
+    //   }
+    // });
+    document
+      .querySelector('.modal-movie__Watch')
+      .addEventListener('click', handleClickMovieButton);
+    document
+      .querySelector('.modal-movie__queue')
+      .addEventListener('click', handleClickMovieButton);
+  }
 }
+
 ulEll.addEventListener('click', hendlerClickCard);
