6b3223ef0af5555701981ba36596c14fd7c1aced
Roman <rprikhoda@ukr.net>
2023-04-19 01:20:52 +0300

    make header

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -1,39 +1,30 @@
 function theme() {
+  const btn = document.querySelector('.btn_theme_switcher');
+  const body = document.querySelector('body');
+  const iconMoon = document.querySelector('#icon-moon1');
+  const iconSun = document.querySelector('#icon-sun1');
 
-const btn = document.querySelector('.btn-theme-switcher');
-const body = document.querySelector('body')
-const iconMoon = document.querySelector('#icon-moon1')
-const iconSun = document.querySelector('#icon-sun1')
-    
-    console.log(btn);
-
-btn.addEventListener('click', (e) => {
-    
+  btn.addEventListener('click', e => {
     e.preventDefault();
 
     if (body.hasAttribute('dark')) {
-        body.removeAttribute('dark', '');
-        iconMoon.classList.add('visually-hidden');
-        iconSun.classList.remove('visually-hidden');
-        localStorage.removeItem('theme', 'dark');
-
+      body.removeAttribute('dark', '');
+      iconMoon.classList.add('visually-hidden');
+      iconSun.classList.remove('visually-hidden');
+      localStorage.removeItem('theme', 'dark');
     } else {
-        body.setAttribute('dark', '');
-        iconMoon.classList.remove('visually-hidden');
-        iconSun.classList.add('visually-hidden');
-        localStorage.setItem('theme', 'dark');
-
+      body.setAttribute('dark', '');
+      iconMoon.classList.remove('visually-hidden');
+      iconSun.classList.add('visually-hidden');
+      localStorage.setItem('theme', 'dark');
     }
+  });
 
-    
-})
-
-
-    if (localStorage.getItem('theme') !== null) {
-        body.setAttribute('dark', '');
-        iconMoon.classList.remove('visually-hidden');
-        iconSun.classList.add('visually-hidden');
-    }
+  if (localStorage.getItem('theme') !== null) {
+    body.setAttribute('dark', '');
+    iconMoon.classList.remove('visually-hidden');
+    iconSun.classList.add('visually-hidden');
+  }
 }
-    
-theme(); 
+
+theme();
