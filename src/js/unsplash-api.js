d3707caa311b5925d471fc607e92600e2a712656
Illia Datsenko <datsenko.i@gmail.com>
2023-04-18 18:27:11 +0300

    +

--- src/js/unsplash-api.js
+++ src/js/unsplash-api.js
@@ -23,24 +23,24 @@ export async function handleLoadNextPaginationPage(event) {
   if (inputSearch.value) {
     if (event.type === 'submit') {
       pagination.reset();
-      renderSearch(event, 1);
+      await renderSearch(event, 1).then(data => console.log(data));
     } else {
       const themoviedbAPI = new ThemoviedbAPI();
 
-    themoviedbAPI.query = inputSearch.value.trim();
+      themoviedbAPI.query = inputSearch.value.trim();
 
-    const savedNumberSearchMovie = localStorage.getItem("numberSearchMovie");
-    const parsedSearchMovie = JSON.parse(savedNumberSearchMovie);
-    
-    pagination.setTotalItems(Number(parsedSearchMovie))
+      const savedNumberSearchMovie = localStorage.getItem('numberSearchMovie');
+      const parsedSearchMovie = JSON.parse(savedNumberSearchMovie);
+
+      pagination.setTotalItems(Number(parsedSearchMovie));
       renderSearch(event, pagination._currentPage);
     }
   } else {
     const themoviedbAPI = new ThemoviedbAPI();
     const { data } = await themoviedbAPI.getTrending();
     console.log(data.total_results);
-    initialTotalItems = data.total_results
-    pagination.setTotalItems(initialTotalItems)
+    initialTotalItems = data.total_results;
+    pagination.setTotalItems(initialTotalItems);
 
     renderTrending(pagination._currentPage);
   }
