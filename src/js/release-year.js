1b7331207aac0f5b6d4006f2ec4e0481ab94a941
Illia Datsenko <datsenko.i@gmail.com>
2023-04-18 11:39:04 +0300

    додано рендер queue і watched, + трохи фікс стилів для розмітки карток

--- src/js/release-year.js
+++ src/js/release-year.js
@@ -1,17 +1,26 @@
 export async function makeReleaseYear(movies) {
-          try {
-      
-      movies.results.forEach(element => {
-        const maxLength = 2;
-        const releaseDate = element.release_date;
-        const releaseYear = releaseDate
-        .split('')
-        .slice(0, 4)
-        .join('');
-         element.release_date = releaseYear;
-        return releaseYear;
-      })
-      } catch (err) {
-        console.log(err);
-      }
-    }
+  try {
+    movies.results.forEach(element => {
+      const maxLength = 2;
+      const releaseDate = element.release_date;
+      const releaseYear = releaseDate.split('').slice(0, 4).join('');
+      element.release_date = releaseYear;
+      return releaseYear;
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function makeReleaseYearById(movies) {
+  try {
+    movies.forEach(element => {
+      const releaseDate = element.release_date;
+      const releaseYear = releaseDate.split('').slice(0, 4).join('');
+      element.release_date = releaseYear;
+      return releaseYear;
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
--- src/js/render-library-by-id.js
+++ src/js/render-library-by-id.js
@@ -1,9 +1,9 @@
 import { ThemoviedbAPI } from './themoviedb-api';
-import { changeGenresLength } from './change-genres-length';
-import { makeReleaseYear } from './release-year';
-import { filmNaneLength } from './film-name-length';
+import { changeGenresLengthById } from './change-genres-length';
+import { makeReleaseYearById } from './release-year';
+import { filmNaneLengthById } from './film-name-length';
 import createFilmsCard from '../templates/gallery-card.hbs';
-import { filmPosterLink } from './film-poster-check-link';
+import { filmPosterLinkById } from './film-poster-check-link';
 
 const galleryListEl = document.querySelector('.film__gallery');
 
@@ -21,27 +21,27 @@ export async function renderCardsById(idArray) {
   });
   const cards = await Promise.all(arrayOfPromises); // параллельній запрос!
   console.log(cards);
-  // await filmNaneLength(cards);
-  // await filmPosterLink(cards);
-  // await changeGenresLength(cards);
-  // await makeReleaseYear(cards);
+  await filmNaneLengthById(cards);
+  await filmPosterLinkById(cards);
+  await changeGenresLengthById(cards);
+  await makeReleaseYearById(cards);
   galleryListEl.innerHTML = createFilmsCard(cards);
 }
 
 export function onWatchedBtnClick() {
   let ids = [
-    948713, 594767, 700391, 76600, 502356, 603692, 842675, 640146, 736790,
-    800787, 739405, 1073413, 677179, 1011367, 719256, 1067282, 1110586, 1070777,
-    830896, 493529,
+    948713, 811913, 594767, 700391, 76600, 502356, 603692, 842675, 640146,
+    736790, 800787, 739405, 1073413, 677179, 1011367, 719256, 1067282, 1110586,
+    1070777, 830896, 493529,
   ];
   renderCardsById(ids);
 }
 
 export function onQueueBtnClick() {
   let ids = [
-    948713, 594767, 700391, 76600, 502356, 603692, 842675, 640146, 736790,
-    800787, 739405, 1073413, 677179, 1011367, 719256, 1067282, 1110586, 1070777,
-    830896, 493529,
+    603692, 948713, 493529, 811913, 594767, 700391, 76600, 502356, 603692,
+    842675, 640146, 736790, 800787, 739405, 1073413, 677179, 1011367, 719256,
+    1067282, 1110586, 1070777, 830896, 493529, 1073413,
   ];
   renderCardsById(ids);
 }
