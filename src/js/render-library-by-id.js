da2ba44e32a54a16280738211c71081877399b6f
Illia Datsenko <datsenko.i@gmail.com>
2023-04-19 14:16:01 +0300

    pagination for library

--- src/js/render-library-by-id.js
+++ src/js/render-library-by-id.js
@@ -1,3 +1,6 @@
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+
 import { ThemoviedbAPI } from './themoviedb-api';
 import { changeGenresLengthById } from './change-genres-length';
 import { makeReleaseYearById } from './release-year';
@@ -12,7 +15,47 @@ import { renderLocaleWatch } from './renderStorageWatched';
 
 const galleryListEl = document.querySelector('.film__gallery');
 
-export async function renderCardsById(idArray) {
+const mainPaginationElements = document.querySelector('.js_main_pagination');
+const libraryPaginationElements = document.querySelector(
+  '.js_library_pagination'
+);
+
+let libraryPagination;
+let libraryArray;
+let libPaginationOptions;
+
+export function createPaginationForLibrary(array) {
+  libPaginationOptions = {
+    totalItems: array.length,
+    itemsPerPage: 20,
+    visiblePages: 5,
+    page: 1,
+  };
+  libraryPagination = new Pagination('search-pagination', libPaginationOptions);
+  libraryArray = array;
+  mainPaginationElements.classList.add('is-hidden');
+}
+
+export function onLibPaginationPageClick() {
+  libraryPagination.on('afterMove', event => {
+    const currentPage = event.page;
+    let beginCard = libPaginationOptions.itemsPerPage * (currentPage - 1);
+    let endCard = libPaginationOptions.itemsPerPage * currentPage;
+    renderCardsByIdWithPagination(libraryArray.slice(beginCard, endCard));
+
+    console.log(currentPage);
+  });
+}
+
+export function renderCardsById(idArray) {
+  console.log(idArray);
+  createPaginationForLibrary(idArray);
+  renderCardsByIdWithPagination(
+    idArray.slice(0, libPaginationOptions.itemsPerPage)
+  );
+}
+
+export async function renderCardsByIdWithPagination(idArray) {
   const themoviedbAPI = new ThemoviedbAPI();
 
   const arrayOfPromises = idArray.map(async id => {
@@ -76,3 +119,30 @@ export function onQueueBtnClick() {
     }
   });
 }
+
+// export function onQueueBtnClick() {
+//   onAuthStateChanged(auth, user => {
+//     if (user === null) {
+//       // если не залогинился рендерим массив из localstoge
+//       let ids = [
+//         603692, 948713, 493529, 811913, 594767, 700391, 76600, 502356, 603692,
+//         842675, 640146, 736790, 800787, 739405, 1073413, 677179, 1011367,
+//         719256, 1067282, 1110586, 1070777, 830896, 493529, 1073413, 603692,
+//         948713, 493529, 811913, 594767, 700391, 76600, 502356, 603692, 842675,
+//         640146, 736790, 800787, 739405, 1073413, 677179, 1011367, 719256,
+//         1067282, 1110586, 1070777, 830896, 493529, 1073413,
+//       ];
+//       const arrData = renderLocaleQueue();
+//       // createPaginationForLibrary(ids);
+//       // console.log(ids);
+//       renderCardsById(ids);
+//     } else {
+//       // если юзер вошел (логин, пароль) рендерим с сервера
+//       getUserData(getArrayOfIdMovies);
+
+//       function getArrayOfIdMovies(data) {
+//         renderCardsById(data.queue);
+//       }
+//     }
+//   });
+// }
