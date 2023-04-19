913123ea72ecf47311134d6a94b06c5e4bc1a774
PopovaAnna1209 <popovaanna120987@gmail.com>
2023-04-19 12:17:15 +0300

    прелоадер в поиске
    убрала вообще, так хоть поиск работает

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -5,18 +5,30 @@ document.body.onload = function () {
         }
 }
 
+const content = document.querySelector('content')
+content.onload = function () {
+    const preloader = document.getElementById('preloader');
+    if (!preloader.classList.contains('done')) {
+        preloader.classList.add('done');
+    }
+}
+
+
 export function showPreloader () {
     const preloader = document.querySelector('preloader');
     preloader.classList.contains('done') && preloader.classList.remove('done');
+    console.log('запустился')
   }
+  
   
   export function hidePreloader() {
     const preloader = document.querySelector('preloader');
     const iframe = document.querySelector('iframe');
-    const img = document.querySelector('.film__img__img');
+    const img = document.querySelector('.film__img');
     iframe
       ? iframe.onload = function () { !preloader.classList.contains('done') && preloader.classList.add('done') }
       : img
       ? img.onload = function () { !preloader.classList.contains('done') && preloader.classList.add('done') }
       : !preloader.classList.contains('done') && preloader.classList.add('done')
+      console.log('остановился')
   }
